c607e9091cb382349d0a7660999364003d15929c
root <henriquiui70@gmail.com>
2018-03-23 00:53:17 -0300

    criando pasta

---
+++ sourcecode/classesVocabulario.cpp
@@ -0,0 +1,27 @@
+#include <string>
+
+class NOME {
+
+    private:
+    string nome;
+
+    public:
+    void RecebeNome (string nome) {
+        this->nome = nome;  //Como manipular strings em c++?
+    }; //Inline escolhido devido ao pequeno tamanho do metodo
+
+}
+
+class SOBRENOME {
+
+    private:
+    string sobrenome;
+
+    public:
+    void RecebeSobrenome (string sobrenome) {
+        this->sobrenome = nome;
+    }
+
+}
+
+// Indicar material a ser estudado para a manipulacao de strings
