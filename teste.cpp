3389a813bcb85548ccd65bacb7b0cc367ae17659
Gabriel Alves <gabriel_alvesozorio@hotmail.com>
2018-03-22 21:37:22 -0300

    zoeira

---
+++ teste.cpp
@@ -0,0 +1,13 @@
+class gay {
+    private:
+    int quantosViadosVaoLEREssaMErda;
+
+    public:
+    void incrementa(int);
+}
+
+gay::quantosViadosVaoLEREssaMErda = 0;
+
+gay::incrementa(int valor) {
+    this->quantosViadosVaoLEREssaMErda++;
+}
