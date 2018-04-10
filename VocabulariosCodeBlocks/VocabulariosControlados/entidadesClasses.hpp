78bc7d92b17810d9fc510af8a7283b037c2375ab
Haori1 <henriquiui70@gmail.com>
2018-04-09 23:41:56 -0300

    Validações dos dominios concluidas, Teste de unidades dos dominios concluidas

--- VocabulariosCodeBlocks/VocabulariosControlados/entidadesClasses.hpp
+++ VocabulariosCodeBlocks/VocabulariosControlados/entidadesClasses.hpp
@@ -13,6 +13,26 @@ class Leitor{
         Sobrenome sobrenome;
         Correio_Eletronico correio_eletronico;
         Senha senha;
+    public:
+        void setNome(const Nome &nome){
+            this->nome = nome;
+        }
+        Nome getNome() const {
+            return nome;
+        }
+        void setSobrenome(const Sobrenome &sobrenome){
+            this->sobrenome = sobrenome;
+        }
+        Sobrenome getSobrenome() const {
+            return sobrenome;
+        }
+        void setCorreio_Eletronico(const Correio_Eletronico &correio_eletronico){
+            this->correio_eletronico = correio_eletronico;
+        }
+        Correio_Eletronico getCorreio_Eletronico() const {
+            return correio_eletronico;
+        }
+        void setSenha(string senha) throw (invalid_argument);
 
 };
 
@@ -20,7 +40,36 @@ class Desenvolvedor{
     private:
         Nome nome;
         Sobrenome sobrenome;
-        
+        Data data;
+        Correio_Eletronico correio_eletronico;
+        Senha senha;
+    public:
+        void setNome(const Nome &nome){
+            this->nome = nome;
+        }
+        Nome getNome() const {
+            return nome;
+        }
+        void setSobrenome(const Sobrenome &sobrenome){
+            this->sobrenome = sobrenome;
+        }
+        Sobrenome getSobrenome() const {
+            return sobrenome;
+        }
+        void setData(const Data &data){
+            this->data = data;
+        }
+        Data getData() const {
+            return data;
+        }
+        void setCorreio_Eletronico(const Correio_Eletronico &correio_eletronico){
+            this->correio_eletronico = correio_eletronico;
+        }
+        Correio_Eletronico getCorreio_Eletronico() const {
+            return correio_eletronico;
+        }
+        void setSenha(string senha) throw (invalid_argument);
+
 };
 
 
